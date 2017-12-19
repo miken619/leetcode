3492471beefed20a76a42c4b7708d26a4f49b322
Michael <miken619@gmail.com>
2017-12-19 13:47:46 -0800

    Solved 661 easy java

---
+++ easy/Java/661.java
@@ -0,0 +1,50 @@
+/*Given a 2D integer matrix M representing the gray scale of an image, you need to design a smoother to make the gray scale of each cell becomes the average gray scale (rounding down) of all the 8 surrounding cells and itself. If a cell has less than 8 surrounding cells, then use as many as you can.
+
+Example 1:
+Input:
+[[1,1,1],
+ [1,0,1],
+ [1,1,1]]
+Output:
+[[0, 0, 0],
+ [0, 0, 0],
+ [0, 0, 0]]
+Explanation:
+For the point (0,0), (0,2), (2,0), (2,2): floor(3/4) = floor(0.75) = 0
+For the point (0,1), (1,0), (1,2), (2,1): floor(5/6) = floor(0.83333333) = 0
+For the point (1,1): floor(8/9) = floor(0.88888889) = 0
+Note:
+The value in the given matrix is in the range of [0, 255].
+The length and width of the given matrix are in the range of [1, 150].*/
+
+class 661 {
+    public int[][] imageSmoother(int[][] M) {
+        int[] xPos = {-1, 1, 0, 0, -1, -1, 1, 1, 0};
+        int[] yPos = {0, 0, -1, 1, -1, 1, -1, 1, 0};
+        int direction = xPos.length;
+        int n = M.length, m = M[0].length; 
+        
+        int[][] result = new int[n][m];
+        
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < m; ++j) {
+                int cells = 0;
+                int sum = 0;
+                for(int k = 0; k < direction; ++k) {
+                    int x = i, y = j;    
+                    x += xPos[k];
+                    y += yPos[k];
+                   if(x >= 0 && x < n && y >= 0 && y < m) {
+                         sum += M[x][y];
+                         ++cells;
+                    }
+                }
+                sum /= cells;
+                result[i][j] = sum;
+            }
+        }
+        
+        return result;
+        
+    }
+}
