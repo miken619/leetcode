153646df3738b8ce37d68c33f2a778e18b9bfd9d
Michael <miken619@gmail.com>
2017-12-30 17:29:35 -0800

    Solved 066 easy java

---
+++ easy/Java/066.java
@@ -0,0 +1,24 @@
+/*Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
+
+You may assume the integer do not contain any leading zero, except the number 0 itself.
+
+The digits are stored such that the most significant digit is at the head of the list.
+
+*/
+
+class 066 {
+    public int[] plusOne(int[] digits) {
+        int length = digits.length;
+        for(int i =length - 1; i >= 0; --i) {
+           if(digits[i] < 9) {
+               ++digits[i];
+               return digits;
+           }
+           digits[i] = 0;
+        }  
+        
+        int[] result = new int[length + 1];
+        result[0] = 1;
+        return result;    
+    }
+}
