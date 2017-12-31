5c3f11922f7b1311b40617868883a7378defbf7a
Michael <miken619@gmail.com>
2017-12-30 18:58:14 -0800

    Solved 459 easy java

---
+++ easy/Java/459.java
@@ -0,0 +1,41 @@
+/*Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together. You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
+Example 1:
+Input: "abab"
+
+Output: True
+
+Explanation: It's the substring "ab" twice.
+Example 2:
+Input: "aba"
+
+Output: False
+Example 3:
+Input: "abcabcabcabc"
+
+Output: True
+
+Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
+*/
+
+class 459 {
+    public boolean repeatedSubstringPattern(String s) {
+      int length = s.length();
+      
+      for(int i = length/2; i >= 1; --i) {
+          if(length%i == 0) {
+              int m = length/i;
+              String substring = s.substring(0, i);
+              StringBuilder sb = new StringBuilder();
+              for(int j = 0; j < m; ++j) {
+                  sb.append(substring);
+              }
+              if(sb.toString().equals(s)) return true;
+          }
+      }
+    
+      return false;
+        
+      // String s = str + str;
+      // return s.substring(1, s.length() - 1).contains(str);
+    }
+}
