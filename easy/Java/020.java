459f55723af985f0179a6a0b7d6b1296614b6315
Michael <miken619@gmail.com>
2017-12-31 20:43:30 -0800

    Solved 020 easy java

---
+++ easy/Java/020.java
@@ -0,0 +1,19 @@
+/*Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.*/
+
+class 020 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(char ch: s.toCharArray()) {
+            if(ch == '(' || ch == '{' || ch == '[') stack.push(ch);
+            else if(!stack.isEmpty()){
+                char temp = stack.pop();
+                if(temp == '(' && ch != ')' || temp == '{' && ch != '}' || temp == '[' && ch != ']') return false;
+            }else return false;
+        }
+        
+        return stack.isEmpty();
+    }
+}
