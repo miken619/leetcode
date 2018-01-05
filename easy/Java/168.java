65111ceec9af10f106225c87acad2505efaca602
Michael <miken619@gmail.com>
2018-01-04 16:18:15 -0800

    Solved 168 easy java

---
+++ easy/Java/168.java
@@ -0,0 +1,26 @@
+/*
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+For example:
+
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB */
+
+    class Solution {
+        public String convertToTitle(int n) {
+            StringBuilder sb = new StringBuilder();
+            
+            while(n > 0) {
+                n--;
+                sb.insert(0, (char)('A' + n % 26));
+                n /= 26;
+            }
+            
+            return sb.toString();     
+        }
+    }
