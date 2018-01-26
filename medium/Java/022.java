663a6da102fb79aeb65c578629d815dad9cd3af6
Michael <miken619@gmail.com>
2018-01-25 16:24:47 -0800

    Solved 022 medium java

--- medium/Java/022.java
+++ medium/Java/022.java
@@ -11,3 +11,24 @@ For example, given n = 3, a solution set is:
 ]
 */
 
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        generateParenthesisUtil(result, "", 0, 0, n);
+        return result;
+    }
+    
+    public void generateParenthesisUtil(List<String> result, String str, int open, int close, int max) {
+        if(str.length() == max * 2) {
+            result.add(str);
+            return;
+        }
+        
+        if(open < max) {
+            generateParenthesisUtil(result, str + "(", open + 1, close, max);
+        }
+        if(close < open) {
+            generateParenthesisUtil(result, str + ")", open, close + 1, max);
+        }
+    }
+}
